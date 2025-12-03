c41da3722fc7ff927cb2cc3aabd3ea5ab02b80e0
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-12-03 08:58:09 +0500

    Add project files.

---
+++ Tyuiu.HaevGS.Sprint6.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.HaevGS.Sprint6.Task0.V2.Lib;
+
+namespace Tyuiu.HaevGS.Sprint6.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 6.695;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
