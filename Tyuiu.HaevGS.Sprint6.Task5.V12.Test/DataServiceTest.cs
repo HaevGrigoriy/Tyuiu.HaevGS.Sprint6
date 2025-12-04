2f27c48db98f43732b473aed7890e4f057617bf3
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-12-04 10:37:34 +0500

    Task 5

---
+++ Tyuiu.HaevGS.Sprint6.Task5.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.HaevGS.Sprint6.Task5.V12.Lib;
+
+namespace Tyuiu.HaevGS.Sprint6.Task5.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string filePath = @"C:\\Users\\ananas\\source\\repos\\tyuiu.cources.programming.files-main\\Sprint6Task5\\InPutDataFileTask5V25.txt";
+
+            var res = ds.LoadFromDataFile(filePath);
+            int wait = 3;
+            Assert.Equals(wait, res);
+        }
+    }
+}
